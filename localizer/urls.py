6f187aa425b6294f8f7fd9a10a25ed68bba394aa
Ekaterina <ek.stupina@gmai.com>
2017-09-16 20:16:07 +0300

    Probe html now automatically records and uploads audio file

--- localizer/urls.py
+++ localizer/urls.py
@@ -6,5 +6,7 @@ app_name = 'localizer'
 urlpatterns = [
     url(r'^$', views.file_choice, name='file_choice'),
     url(r'recorderjs_test/', views.recorderjs_test, name="recorderjs_test"),
-    url(r'probe/', views.probe, name='probe')
+    url(r'probe/$', views.probe, name='probe'),
+    url(r'probe/upload/$', views.upload_probe, name='upload_probe'),
+
 ]
--- localizer/views.py
+++ localizer/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
 from .forms import UploadFileForm
+from .models import Response
 
 def file_choice(request):
     if request.method == 'POST':
@@ -17,5 +18,16 @@ def file_choice(request):
 def recorderjs_test(request):
     return render(request, 'localizer/waveRecorder.html')
 
+
 def probe(request):
-    return render(request, 'localizer/probe.html')
+    form = UploadFileForm()
+    return render(request, 'localizer/probe.html', {'form': form})
+
+def upload_probe(request):
+    if request.method == 'POST':
+        with open("responses/recording2.ogg", "wb") as f:
+            f.write(request.body)
+
+        return HttpResponse("Sound uploaded")
+    else:
+        return HttpResponse("View should only be requested with POST")
