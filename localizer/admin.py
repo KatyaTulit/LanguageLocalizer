1c41837fc5fc46bd5f05709c58c8ad8552046356
Ekaterina <ek.stupina@gmai.com>
2017-11-13 22:52:10 +0300

    Added ModelAdmin for Subject and Answer

--- localizer/admin.py
+++ localizer/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Subject, Answer
 
-admin.site.register(Subject)
-admin.site.register(Answer)
 
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('code_name', 'probe_control_conditions', 'unique_id', 'finished')
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('probe', 'subject', 'response_sound_file_path')
+
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Answer, AnswerAdmin)
