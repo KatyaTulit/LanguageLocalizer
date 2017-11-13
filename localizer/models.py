a418bc958e1b10009891a1336ce6d4ed6a079b7b
Ekaterina <ek.stupina@gmai.com>
2017-11-13 23:06:42 +0300

    changed condition randomization rules

--- localizer/models.py
+++ localizer/models.py
@@ -113,7 +113,9 @@ class Subject(models.Model):
 
         age_group = [group for group in self.AGE_GROUPS if group[0] <= self.age <= group[1]][0]
         finished_counts = [len(Subject.objects.filter(finished=True, probe_control_conditions=cond,
-                                                      age__gte=age_group[0], age__lte=age_group[1]))
+                                                      age__gte=age_group[0], age__lte=age_group[1])
+                                              .exclude(code_name__startswith='fail')
+                               )
                            for cond in self.CONDITIONS]
 
         if finished_counts[0] == finished_counts[1]:
